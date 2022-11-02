d1898a444cce671fcc9c2f80cb7bacf94beedc50
bonzer75 <bonzer-noreply@fedora.email>
2022-11-02 12:37:48 -0500

    added uberduck function

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
-import shutil, os, uberduck
+import shutil, os
+import uberduckapi as ub
 from gtts import gTTS
 from dotenv import load_dotenv
-from io import StringIO, BytesIO
 
 load_dotenv()
 
@@ -28,25 +28,12 @@ def googleTTS(message, len):
     speech = gTTS(text = arg, lang = len, slow = False)
     speech.save("./files/audio.mp3")
 
-def uberduck(message):
-    client = uberduck.UberDuck(os.getenv('KEY'), os.getenv('SECRET'))
-    voices = uberduck.get_voices(return_only_names = True)
+def uberduck(message, voice):
+    duck = ub.UberDuck(os.getenv('KEY'), os.getenv('SECRET'))
+    sponge = duck.get_voice(voice, message)
 
-    speech = input('Enter speech: ')
-    voice = input('Enter voice or enter "LIST" to see list of voices: ')
-
-    if voice == 'LIST':
-        print('Available voices:\n')
-        for voice in sorted(voices): # sorting the voice list in alphabetical order
-            print(voice)
-        exit()
-
-    if voice not in voices:
-        print('Invalid voice')
-        exit()
-
-    result = client.speak(speech, voice, check_every = 0.5)
-    return print(BytesIO(result))
+    if sponge:
+        return sponge.save('./files/audio.mp3')
 
 #requires token
 def msTTS(message):
