fe2eb247c78a2f5534f2353fecbc6ac36c057ff4
bonzer75 <haroldveracs@gmail.com>
2022-11-13 15:04:39 -0500

    uberduck command support for custom server files

--- cogs/uberduck.py
+++ cogs/uberduck.py
@@ -10,19 +10,20 @@ class Uberduck(commands.Cog):
 
     @commands.command()
     async def uberduck(self, ctx, arg1, arg2):
-        print("Started command")
+        server = ctx.guild.id
         #check if bot is talking
         if (ctx.voice_client is not None) and (ctx.voice_client.is_playing()):
             return await ctx.send("Espera a que termine de hablar")
 
         if "@" in arg2:
             return await ctx.send("No me hagas mencionar a usuarios, por favor")
+        
         print("Checks done")
-        uberduckTTS(arg2, arg1) #rick-sanchez
+        uberduckTTS(arg2, arg1, server) #rick-sanchez
         print("Uberduck file")
         await ctx.invoke(self.bot.get_command('join'))
         print("Playing audio")
-        return ctx.voice_client.play(FFmpegPCMAudio("./files/audio.mp3"))
+        return ctx.voice_client.play(FFmpegPCMAudio(f"./files/{server}/audio.mp3"))
 
 async def setup(bot):
     await bot.add_cog(Uberduck(bot))
