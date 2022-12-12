446d4a919615f1af0f7f9a394f5d336e1e15e8c7
bonzer <haroldv@tuta.io>
2022-12-12 16:45:46 -0500

    stupid fix

--- functions.py
+++ functions.py
@@ -47,7 +47,7 @@ def uberduckTTS(message, voice, server):
     if sponge:
         return sponge.save(f'./files/{server}/audio.mp3')
     else:
-        return f"{language["uberduck"]["error1"]}"
+        return language["uberduck"]["error1"]
 
 #requires token
 def msTTS(message):
