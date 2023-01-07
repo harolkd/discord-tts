bd6bcfca67a73a1042bba77a75f3240f834b9c9c
bonzer75 <bonzer-noreply@fedora.email>
2023-01-06 22:05:31 -0500

    trying to add autoleave

---
+++ cogs/auto_leave.py
@@ -0,0 +1,17 @@
+import discord
+from main import config
+from discord.ext import commands
+
+class Voice(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
+        if before.channel:
+            if before.channel.name == "":
+                if len(before.channel.members) == 0:
+                    await ctx.invoke(self.bot.get_command('leave'))
+
+async def setup(bot):
+    await bot.add_cog(Voice(bot))
